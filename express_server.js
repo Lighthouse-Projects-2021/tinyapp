d47edd72d789116476fce91e7589d7d4359f742d
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-27 23:28:49 +0000

    Updated render urls_index

--- express_server.js
+++ express_server.js
@@ -13,13 +13,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
